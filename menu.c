2b8d44729f448489b6a1000e07cd41c767fe77b8
almudenacostes <a.costes@alumnos.upm.es>
2019-05-08 10:24:18 +0200

    hemos continuado el codigo

--- menu.c
+++ menu.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<string.h>
 #define N 100
 
 
@@ -14,12 +15,13 @@ int main()
 	// sea a sacar efectivo,b ingresar efectivo,c cambiar el pin,d consultar saldo y e salir 
     int a,b,c,d,e;
     int operacion;
-    int pintarjeta,pin,np,cp,intentos=0;
+    int pintarjeta,pin,np,cp,intentos=0,i;
     //sea pintarjeta el pin de la tarjeta, pin el pin introducido, sea np nuevo pin, sea cp cambio de pin, intentos.
     //he asignado cantidad1 a la cantidad de dinero que desea retirar y cantidad2 a la cantidad de dinero que se desea ingresar.
     int cantidad1,cantidad2;
-	int clic=0; 
+	int clic=0,compara; 
 	float saldo;
+	char nombredelcliente[17];
 	tarjeta clientes[N]; 
 	printf ("Introduzca la tarjeta\n");
     scanf ("%i",&numerotarjeta);
@@ -45,39 +47,62 @@ int main()
 	}
 
 	fclose(clientesdelbanco);
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    {
-    do{ 
-			printf("Introduzca el pin: "); 
-			scanf("%d", &pin); 
-			if(pin==pintarjeta) 
-			printf("Bienvenido");
-			intentos++;  
-			}while (intentos<3 && pin!=pintarjeta);
-	
+	printf("Iniciar sesion:\n");
+    if(clic==0)
+					{
+						printf("No hay ningun cliente registrado en el banco.");
+						break;
+					}
+					else
+					{
+					clientesdelbanco=fopen("clientesdelbanco.txt","r");
+						if(clientesdelbanco==NULL)
+						{
+						printf("El fichero no se pudo abrir\n");
+						return -1;
+						}
+						else
+						{
+							i=0;
+							do
+							{
+								do
+								{
+								printf("Introduzca su usuario(nombre del cliente):\n");
+								scanf("%s",nombredelcliente);
+								printf("\n");
+								for(i=0;i<clic;i++)
+									{
+									compara=strcmp(nombredelcliente,clientes[i].nombre);
+										if(compara==0)
+										{
+										printf("El usuario es correcto.\n");
+										break;
+										}
+										else if(i==clic-1)
+										{
+											printf("Su usuario es incorrecto.\nPruebe otra vez\n\n");
+										}
+									}
+								}while(compara==1||compara==-1);
+							    do{ 
+										printf("Introduzca el pin: "); 
+										scanf("%d", &pin); 
+										if(pin==pintarjeta) 
+										printf("Bienvenido");
+										intentos++;  
+										}while (intentos<3 && pin!=pintarjeta);
+							fclose(registro);
+
+							}while(fscanf(registro,"%i  %s\t%i\t%i\t%f\t",&cont1,usuarios[cont1].nombre,&usuarios[cont1].DNI,&usuarios[cont1].pin,&usuarios[cont1].dinero)!=EOF);
+
+						}
+					}	
+    do{
+
 	printf("Seleccione si desea sacar efectivo(a),ingresar efectivo(b),cambiar el pin(c),consultar saldo(d) o salir(e)");
 	scanf("%i",&operacion)
-	
-	
+
 	switch (operacion) 
 		{ 
 		case `a`: 
@@ -119,6 +144,12 @@ int main()
 		case `e`:
 					printf("¡Hasta pronto!");
 		 break;
+		 default:
+
+				printf("Opcion incorrecta\n");
+		}
+
+	}while(opcion!=6);
 	 }
 }
 
