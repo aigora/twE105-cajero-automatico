eacd4aa05ce90f0da89f6db592fcbe00061c16dd
almudenacostes <a.costes@alumnos.upm.es>
2019-05-11 09:13:58 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -4,7 +4,7 @@
 
 
 typedef struct{
-	char nombrecliente;
+	char nombrecliente[N];
 	int numerotarjeta,pintarjeta;
 	float saldo;
 }tarjeta;
@@ -13,24 +13,14 @@ typedef struct{
 int main()
 {
 	// sea a sacar efectivo,b ingresar efectivo,c cambiar el pin,d consultar saldo y e salir 
-    const char a,b,c,d,e;
-    int operacion;
-    int pintarjeta,pin,np,cp,intentos=0,i,nombre;
-    //sea pintarjeta el pin de la tarjeta, pin el pin introducido, sea np nuevo pin, sea cp cambio de pin, intentos.
-    //he asignado cantidad1 a la cantidad de dinero que desea retirar y cantidad2 a la cantidad de dinero que se desea ingresar.
-    int cantidad;
+    int operacion;//para seleccionar la opcion que deseas realizar.
     int pintarjeta,pin,np,cp,intentos=0,i;
     //sea pintarjeta el pin de la tarjeta, pin el pin introducido, sea np nuevo pin, sea cp cambio de pin, intentos.
-    //he asignado cantidad1 a la cantidad de dinero que desea retirar y cantidad2 a la cantidad de dinero que se desea ingresar.
-    int cantidad1,cantidad2;
+    //he asignado cantidad a la cantidad de dinero que desea retirar y a la cantidad de dinero que se desea ingresar.
+    int cantidad;
 	int clic=0,compara; 
-	float saldo;
 	char nombredelcliente[17];
 	tarjeta clientes[N]; 
-    FILE *clientesdelbanco;
-
-	printf ("Introduzca la tarjeta\n");
-    scanf ("%i",&numerotarjeta);
     FILE *clientesdelbanco;
     //introduce el pin y en caso de que sea incorrecto tiene 3 oportunidades y sino lo acierta fin del programa.
 
@@ -54,30 +44,27 @@ int main()
 	}
 
 	fclose(clientesdelbanco);
-	printf("Iniciar sesion:\n");
-    if(clic==0)
-
-						printf("No hay ningun cliente registrado en el banco.");
-
+	do
+		{
+				printf("Bienvenido.");
+				printf("Iniciar sesion:\n");
+			do
+			{
+				if(clic==0)
 					{
-						printf("No hay ningun cliente registrado en el banco.");
-						break;
+						printf("Todavia no hay ningun usuario registrado en el cajero. Cree un usuario antes.");
 					}
 					else
 					{
-					clientesdelbanco=fopen("clientesdelbanco.txt","r");
+						 clientesdelbanco = fopen("clientesdelbanco.txt","r");
 						if(clientesdelbanco==NULL)
 						{
-						printf("El fichero no se pudo abrir\n");
-						return -1;
+							printf("El fichero no se pudo abrir\n");
+							return -1;
 						}
 						else
 						{
 							i=0;
-							do
-							{
-								do
-								{
 								printf("Introduzca su usuario(nombre del cliente):\n");
 								scanf("%s",nombredelcliente);
 								printf("\n");
@@ -87,12 +74,6 @@ int main()
 										if(compara==0)
 										{
 										printf("El usuario es correcto.\n");
-										nombre=i;
-									compara=strcmp(nombredelcliente,clientes[i].nombre);
-										if(compara==0)
-										{
-										printf("El usuario es correcto.\n");
-										break;
 										}
 										else if(i==clic-1)
 										{
@@ -100,8 +81,7 @@ int main()
 										}
 									}
 								}while(compara==1||compara==-1);
-							    do{     //introduce el pin y en caso de que sea incorrecto tiene 3 oportunidades y sino lo acierta fin del programa.
-
+							       //introduce el pin y en caso de que sea incorrecto tiene 3 oportunidades y sino lo acierta fin del programa.
 							    do{ 
 										printf("Introduzca el pin: "); 
 										scanf("%d", &pin); 
@@ -111,15 +91,8 @@ int main()
 										}while (intentos<3 && pin!=pintarjeta);
 							fclose(clientesdelbanco);
 
-							}while(fscanf(clientesdelbanco,"%i  %s\t%i\t%i\t%f\t",&clientes[clic].nombrecliente,clientes[clic].numerotarjeta,&clientes[clic].pintarjeta,&clientes[clic].saldo)!=EOF);
-
-							fclose(registro);
-
-							}while(fscanf(registro,"%i  %s\t%i\t%i\t%f\t",&cont1,usuarios[cont1].nombre,&usuarios[cont1].DNI,&usuarios[cont1].pin,&usuarios[cont1].dinero)!=EOF);
-
-						}
-					}	
-    do{
+							}while (fscanf(clientesdelbanco,"%i  %s\t%i\t%i\t%f\t",&clic,clientes[clic].nombrecliente,&clientes[clic].numerotarjeta,&clientes[clic].pintarjeta,&clientes[clic].saldo)!=EOF);
+						
 
 	printf("Seleccione si desea sacar efectivo(a),ingresar efectivo(b),cambiar el pin(c),consultar saldo(d) o salir(e)");
 
@@ -127,44 +100,43 @@ int main()
 
 	switch (operacion) 
 		{ 
-		case `a`: 
+		case 1:
 		     do{ 
 					printf("introducir cantidad que desea retirar:"); 
 					scanf("%d", &cantidad);
-					if(cantidad1<=saldo)
+					if(cantidad<=clientes[i].saldo)
 					{
 				    clientes[i].saldo=clientes[i].saldo-cantidad; 
 					printf("Procedamos a la retirada.");
 				    }
 					intentos++;  
-					}while (intentos<3 && cantidad1>saldo);	
+					}while (intentos<3 && cantidad>clientes[i].saldo);	
 		 break; 
-		case `b`:
+		case 2:
 					 printf ("Escriba la cantidad de dinero que desee ingresar");
 					 scanf("%i",&cantidad);
 					 printf("Ingreso realizado con exito");
                      clientes[i].saldo=clientes[i].saldo+cantidad;
-					 printf("su nuevo saldo es %.3f",saldo);	 
+					 printf("su nuevo saldo es %.3f",clientes[i].saldo);	 
 		 break; 
-		case `c`;
+		case 3:
 				     do{ 
 					printf("Escriba su pin actual"); 
 					scanf("%d", &pin); 
 					if(pin==pintarjeta) 
 					    {
 						printf("Introduzca su nuevo pin");
-						scanf("%d",%np);
+						scanf("%d",&np);
 						pintarjeta=np;
 						printf("Su codigo pin se ha cambiado con exito");
 						}
 					intentos++;  
 					}while (intentos<3 && pin!=pintarjeta);
 		 break; 
-		case `d`;
-					printf ("Su saldo actual a dia de hoy es %.3f",nombredelcliente[i].saldo);
+		case 4:
+					printf ("Su saldo actual a dia de hoy es %.3f",clientes[i].saldo);
 		 break;
-		case `e`:
-			{
+		case 5:
 				clientesdelbanco= fopen("clientesdelbanco.txt","w");
 				if(clientesdelbanco==NULL)
 				{
@@ -176,28 +148,22 @@ int main()
 					i=0;
 					while(i<clic)
 					{
-						fprintf(clientesdelbanco,"%i  %s\t%i\t%i\t%.3f\n",i+1,nombredelcliente[i].nombre,nombredelcliente[i].pintarjeta,nombredelcliente[i].saldo);
+						fprintf(clientesdelbanco,"%i  %s\t%i\t%i\t%.3f\n",i+1,clientes[i].nombrecliente,&clientes[i].numerotarjeta,&clientes[i].pintarjeta,&clientes[i].saldo);
 						i++;
 					}
 					fclose(clientesdelbanco);
 					printf("Hasta pronto.\n");	
 				}
 			break;
-			}
-		 default:
-
-					printf("¡Hasta pronto!");
-		 break;
 		 default:
 
 				printf("Opcion incorrecta\n");
+				break;
 		}
 
-	}while(opcion!=6);
-
-	 }
-
+	}
 }
+
 
 
 
